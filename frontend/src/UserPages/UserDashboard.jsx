7b9ad07a0f28793ebc5b769c5fa1e51cd0d3195c
alishaayyyy <alisha12740@gmail.com>
2025-08-07 12:33:30 -0700

    work

--- frontend/src/UserPages/UserDashboard.jsx
+++ frontend/src/UserPages/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import LayoutUser from "./Components/LayoutUser.jsx";
+import LayoutUser from "./Componentss/LayoutUser.jsx";
 import Dashboard from "./pages/Dashboard";
 import QuestionManager from "./pages/Questions";
 import Users from "./pages/User";
