1fd478a2990186305c215fc2df35db574774d8a5
alishaayyyy <alisha12740@gmail.com>
2025-08-07 12:06:14 -0700

    m

--- frontend/src/UserPages/components/Layout.jsx
+++ frontend/src/UserPages/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import Sidebar from "./Sidebar";
-
+import Sidebar from './Sidebar';
 export default function Layout({ children }) {
   const [open, setOpen] = useState(false); // mobile default closed
 
